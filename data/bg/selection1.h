625c7306ee38938bdbdfe65447692a073969caab
Simon Larsen <simonhffh@gmail.com>
2015-09-26 20:47:37 -0400

    Updated selection banners

--- data/bg/selection1.h
+++ data/bg/selection1.h
@@ -1,7 +1,7 @@
-#ifndef __selection1_tiles__
-#define __selection1_tiles__
+#ifndef SELECTION1_MAP_H
+#define SELECTION1_MAP_H
 
-#define selection1_data_length 54
+#define selection1_data_length 33
 extern const unsigned char selection1_data[];
 
 #define selection1_tiles_width 20
--- data/bg/selection2.h
+++ data/bg/selection2.h
@@ -1,7 +1,7 @@
-#ifndef __selection2_tiles__
-#define __selection2_tiles__
+#ifndef SELECTION2_MAP_H
+#define SELECTION2_MAP_H
 
-#define selection2_data_length 62
+#define selection2_data_length 71
 extern const unsigned char selection2_data[];
 
 #define selection2_tiles_width 20
